373eaee2ca936020b2124a26acf8d7095a58423f
abby0301 <j2300008@gunma-u.ac.jp>
2026-03-20 08:45:54 +0900

    Update script.js

--- tanoai_2026/script.js
+++ tanoai_2026/script.js
@@ -6,7 +6,15 @@ function checkCode() {
     const pages = {
         "1234": "image/messageA.jpg",
         "5678": "image/messageB.jpg",
-        "9012": "image/messageC.jpg"
+        "9012": "image/messageC.jpg",
+        "1125": "image/Ihara.png",
+        "1124": "image/Kamegaya.png",
+        "1028": "image/Kitagawa.png",
+        "0304": "image/Sasaki.png",
+        "0512": "image/Yoshida.png",
+        "0320": "image/Kinn.png",
+        "0920": "image/Motoi.png",
+        "0428": "image/Matushita.png"
     };
 
     if (pages[code]) {
